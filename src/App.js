5a758a298dc93e6712efdce077e71e93c23db1c5
sakarbh <sakar811992@gmail.com>
2023-04-11 23:07:01 +0530

    movieland app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css"
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+const url ='http://www.omdbapi.com?apikey=171d48dc'
+const App =() => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [movies , setMovies] = useState([])
+const[searchTerm , setsearchTerm] =useState('')
+    const searchMovies = async (title)=>{
+const response = await fetch(`${url}&s=${title}`)
+const data = await response.json() 
+setMovies(data.Search) 
+
+ }
+ 
+
+ useEffect(()=>{
+    searchMovies('')
+    },[])
+
+    return (
+        <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input
+            placeholder="search for movies"
+            value={searchTerm}
+            onChange={(e)=>{setsearchTerm(e.target.value)}}
+            />
+            <img
+            src={SearchIcon}
+            alt='search'
+            onClick={()=>{searchMovies(searchTerm)}}
+            />
+            </div>
+           {
+            movies?.length>0 ? (
+            <div className="container">
+                {movies.map((movie)=>(
+                    <MovieCard movie={movie}/>
+                ))}
+            </div>
+            )
+            :(
+                <div className="empty">
+                    <h2>No movies found</h2>
+                    </div>
+            )
+        
+           }
+
+        </div>
+        
+    )
+    
 }
 
-export default App;
+
+
+export default App
+//171d48dc
